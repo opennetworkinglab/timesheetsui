24cc97446f3104a510b457b479e494cc5888b71b
Sean Condon <sean@opennetworking.org>
2020-07-13 11:32:06 +0100

    Added in row names to table
    Also added in select boxes for times etc
    
    Added total row and coloured it
    
    Added in colouring for weekends

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -8,12 +8,39 @@ import {Component, Input, OnInit} from '@angular/core';
 export class DayComponent implements OnInit {
   @Input() email: string;
   @Input() day: string;
-  @Input() workedMins: number = 0;
-  @Input() ngpaHours: number = 0;
+  @Input() weekend: boolean;
 
-  constructor() { }
+  @Input() workedMins: number = 0;
+  @Input() ngpaMins: number = 0;
+  @Input() adminMins: number = 0;
+  @Input() fundrMins: number = 0;
+  @Input() salesmMins: number = 0;
+  @Input() otherMins: number = 0;
+  @Input() leavesickMins: number = 0;
+  @Input() leaveptoMins: number = 0;
+  @Input() holidayMins: number = 0;
+  @Input() timeIn1: number = 0;
+  @Input() timeOut1: number = 0;
+  @Input() timeIn2: number = 0;
+  @Input() timeOut2: number = 0;
+  @Input() regMins: number = 0;
+  @Input() otMins: number = 0;
+
+  totalHours: number = 0;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.updateTotal();
+  }
+
+  updateTotal() {
+    this.totalHours = (this.workedMins +
+      this.ngpaMins + this.adminMins +
+      this.fundrMins + this.salesmMins +
+      this.otherMins + this.leavesickMins +
+      this.leaveptoMins + this.holidayMins) / 60;
   }
 
 }
