12886727b9aa89f36e532cba13ac55b9607f1bd3
Valdar <valdar@opennetworking.org>
2021-11-01 17:34:09 +0000

    For approver sign and unsign, it will now update the row and not the entire table. The wait has been removed as well

--- src/app/users-signed/users-signed.component.ts
+++ src/app/users-signed/users-signed.component.ts
@@ -26,6 +26,7 @@ import {APPROVER_NAME_ATTR, EMAIL_ATTR, USERNAME_ATTR} from '../app.component';
 import {MatDialog} from '@angular/material/dialog';
 import {PopupTextComponent} from './popup-text/popup-text.component';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatButton} from '@angular/material/button';
 
 class TempUser{
     email: string;
@@ -229,19 +230,25 @@ export class UsersSignedComponent implements OnInit {
         tempUser.total = 0;
     }
 
-    approverSign(userEmail){
-        this.loadingProgress = true;
-        this.tsweeklyService.signSheetApprover(userEmail, this.currentWeekId).subscribe(result => {
-            this.getUsersAndWeekly();
-            this.loadingProgress = false;
+    approverSign(user, event){
+
+        console.log(event.target);
+        this.tsweeklyService.signSheetApprover(user.email, this.currentWeekId).subscribe(result => {
+
+            user.supervisorSigned = new Date(result.signedDate);
+            user.tsPreview = result.documentUrl;
+
+            this.updateTableRow(user);
         });
     }
 
-    approverUnsign(userEmail){
-        this.loadingProgress = true;
-        this.tsweeklyService.unsignSheetApprover(userEmail, this.currentWeekId).subscribe( result => {
-            this.getUsersAndWeekly();
-            this.loadingProgress = false;
+    approverUnsign(user){
+
+        this.tsweeklyService.unsignSheetApprover(user.email, this.currentWeekId).subscribe( result => {
+
+            user.supervisorSigned = null;
+            user.tsPreview = null;
+            this.updateTableRow(user);
         });
     }
 
@@ -298,6 +305,7 @@ export class UsersSignedComponent implements OnInit {
 
     onReminder(userEmail: string) {
 
+
         const dialog = this.dialog.open(PopupTextComponent, {
             data: {
                 isReminder: true,
@@ -325,6 +333,14 @@ export class UsersSignedComponent implements OnInit {
 
             }, () => {
             });
+    }
+
+    updateTableRow(tempUser) {
+        this.listData.data.find((data, key) => {
+            if (data.email === tempUser.email){
+                data = tempUser;
+            }
+        });
     }
 
     onViewTimesheet(tsPreview: string) {
